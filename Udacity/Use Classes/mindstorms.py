3449f4372e0dec6bf16e293d29dfeafb72d9a239
Oleksandr Vol <ovo@softserveinc.com>
2016-07-26 20:09:06 +0300

    new file added

--- Udacity/Use Classes/mindstorms.py
+++ Udacity/Use Classes/mindstorms.py
@@ -5,7 +5,17 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor('red')
     brad = turtle.Turtle()
+    brad.shape('turtle')
+    brad.color('green')
+    brad.speed(2)
     brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
 
     window.exitonclick()
 
