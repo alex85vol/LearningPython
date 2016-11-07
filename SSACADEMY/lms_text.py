e7cb7e0009ff2ef16027f150ef4a407492956818
alex85vol <alex85vol@gmail.com>
2016-11-07 14:07:09 +0200

    added lms first code

---
+++ SSACADEMY/lms_text.py
@@ -0,0 +1,7 @@
+print "The Zen of Python, by Tim Peters\n\n"\
+      "\rBeautiful is better than ugly.\n"\
+      "\rExplicit is better than implicit.\n"\
+      "\rSimple is better than complex.\n"\
+      "\rComplex is better than complicated.\n"\
+      "\rFlat is better than nested.\n"\
+      "\rSparse is better than dense.\n"
--- SSACADEMY/test1.py
+++ SSACADEMY/test1.py
@@ -1,8 +1,1 @@
 
-def bubbIe_sort (I) :
-    for i in range (len (I)) :
-        for j in range (len (I) – I - 1) :
-            if I [j] > I[j+1] :
-                _____
-
-    return I
